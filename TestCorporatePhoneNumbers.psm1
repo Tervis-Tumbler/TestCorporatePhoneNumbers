21eb6be8ba81e98d6bfc770989c83e8aa5194270
ChrisMagnuson <hourglasssand@gmail.com>
2016-09-20 12:52:15 -0400

    Mature the output of Invoke-WebHookInboxResponder, refactor starting webhookinbox job, add logic to import cell phones from CSV

--- TestCorporatePhoneNumbers.psm1
+++ TestCorporatePhoneNumbers.psm1
@@ -13,7 +13,18 @@ Function Invoke-WebHookInboxResponder {
         $LastCursor = $Response.last_cursor
 
         ForEach ($Item in $Response.Items) {
-            $Item | ConvertTo-Json | Out-File "$OutputDirectory/$($item.id).json"
+            $TwilioCallProperties = $Item.body | ConvertFrom-URLEncodedQueryStringParameterString
+            $TestCorproatePhoneNumberProperties = $Item.query | ConvertFrom-URLEncodedQueryStringParameterString
+
+            $OutputFilePath = "$OutputDirectory/$($TwilioCallProperties.Called)/"
+            New-Item -ItemType Directory -Force -Path $OutputFilePath | Out-Null
+
+            [PSCustomObject][Ordered]@{
+                PhoneNumberTwilioFormat = $TwilioCallProperties.Called
+                UserResponseDateTime = $Item.created
+                MessageType = $TestCorproatePhoneNumberProperties.MessageType
+            } | ConvertTo-Json | Out-File "$OutputFilePath/$($Item.created | get-date -Format -- FileDateTime).json"
+
             New-WebHookInboxResponse -Headers @{"Content-Type"="text/plain"} -ItemID $item.id -body (
                 New-TwiMLResponse -InnerElements (
                     New-TwiMLHangup
@@ -23,6 +34,27 @@ Function Invoke-WebHookInboxResponder {
     }
 }
 
+Function Start-WebHookInboxResponderJob {
+    param (
+        $WebHookInboxID
+    )
+    if (Get-Job -Name WebHookInboxResponder){ 
+        Get-Job -Name WebHookInboxResponder | Stop-Job -PassThru | Remove-Job
+    }
+    Start-Job -Name WebHookInboxResponder -ScriptBlock {param($WebHookInboxID) Invoke-WebHookInboxResponder -WebHookInboxID $WebHookInboxID} -ArgumentList $WebHookInboxID
+}
+
+Function Get-ATTCompanyCellPhones {
+    param (
+        $PathToBasicWirelessUserInventoryReportCSV = $(Import-Clixml $env:USERPROFILE\PathToBasicWirelessUserInventoryReportCSV.txt)
+    )
+    get-content -path $PathToBasicWirelessUserInventoryReportCSV | 
+    Select-Object -Skip 9 |
+    Out-String |
+    ConvertFrom-Csv |
+    Add-Member -Name PhoneNumberTwilioFormat -MemberType ScriptProperty -Value {"+1" + $this."Wireless Number"} -PassThru
+}
+
 Function Test-CorporatePhoneNumbers {
     $TwilioPhoneNumberForOutBoundCall = Get-TwilioIncomingPhoneNumbers | 
     select -ExpandProperty incoming_phone_numbers | 
@@ -30,12 +62,14 @@ Function Test-CorporatePhoneNumbers {
 
     $WebHookInboxResponse = New-WebHookInbox -Response_Mode wait
     Set-WebHookInboxID -WebHookInboxID $WebHookInboxResponse.id
-    Start-Job -ScriptBlock {param($WebHookInboxID) Invoke-WebHookInboxResponder -WebHookInboxID $WebHookInboxID} -ArgumentList $($WebHookInboxResponse.id)
+    Start-WebHookInboxResponderJob -WebHookInboxID $WebHookInboxResponse.id
+    
+    $CompanyCellPhones = Get-ATTCompanyCellPhones | Where 'Wireless User Full Name' -Match "Chris Magnuson"
 
     ForEach ($CompanyCellPhone in $CompanyCellPhones) {
         $URLToConfirmPhoneStillInUse = New-TwilioTwimletSimpleMenuURL -Message "Hello, This is a message from Tervis IT. Please press 1 to confirm you still use this company supplied cell phone." -Options (
             New-TwilioTwimletSimpleMenuOption -Digits 1 -Url (
-                New-TwilioTwimletSimpleMenuURL -Message "If you are $($CompanyCellPhone.EmployeeName) please press 9. If not please press 1" -Options (
+                New-TwilioTwimletSimpleMenuURL -Message "If you are $($CompanyCellPhone."Wireless User Full Name") please press 9. If not please press 1" -Options (
                     (
                         New-TwilioTwimletSimpleMenuOption -Digits 9 -Url (
                             New-TervisTwimletMessageAndRedirectURL -Message "Thank you for confirming your use of this cell phone. Good bye." -URL (
@@ -53,35 +87,8 @@ Function Test-CorporatePhoneNumbers {
             )
         )
 
-        New-TwilioCall -From $TwilioPhoneNumberForOutBoundCall -To $CompanyCellPhone.CellPhoneNumber -Url $URLToConfirmPhoneStillInUse
+        New-TwilioCall -From $TwilioPhoneNumberForOutBoundCall -To $CompanyCellPhone.PhoneNumberTwilioFormat -Url $URLToConfirmPhoneStillInUse
     }
-
-    New-TwilioCall -From $TwilioPhoneNumberForOutBoundCall -To $CompanyCellPhone.CellPhoneNumber -Url (
-        New-TervisTwimletMessageAndRedirectURL -Message "Thank you for confirming your use of this cell phone. Good bye." -URL (
-            New-TestCoropratePhoneNumberWebHookInboxURL -EndCallState IdentityConfirmed
-        )
-    )
-
-    ForEach ($CompanyCellPhone in $CompanyCellPhones) {
-
-        New-WebHookInboxAPIInputURL -QueryStringParamterString "ApplicationName=TestCorporatePhoneNumbers&MessageType=IdentityConfirmed"
-        $MessageURLThankYouForConfirming = New-TwilioTwimletMessageURL -Messages "Thank you for confirming your use of this cell phone. Good bye."
-
-
-        $TwiMLXMLDocument = New-TwiMLXMLDocument -InnerElements $(
-            New-TwiMLResponse -InnerElements $(
-                $(New-TwiMLSay -Message "This is a test"),
-                $(New-TwiMLRedirect -Method post `
-                    -URL $(New-WebHookInboxAPIInputURL -QueryStringParamterString "ApplicationName=TestCorporatePhoneNumbers&MessageType=StatusCallback")
-                )
-            )
-        )
-
-        New-TwilioCall -From $TwilioPhoneNumberForOutBoundCall -To $CompanyCellPhone.CellPhoneNumber -Url $(New-TwilioTwimletEcho -Twiml $TwiMLXMLDocument.OuterXML)
-
-    
-    }
-
 }
 
 
